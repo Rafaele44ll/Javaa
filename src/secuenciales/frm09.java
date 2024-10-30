f47d4fa15a03e01de41ee758a5fe472c92119bb7
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-30 17:45:21 -0500

    java

--- src/secuenciales/frm09.java
+++ src/secuenciales/frm09.java
@@ -1,5 +1,76 @@
 package secuenciales;
 
-public class frm09 {
+import java.awt.EventQueue;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class frm09 extends JFrame {
+    private static final long serialVersionUID = 1L;
+    JTextField txtNumero, txtSuma;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    frm09 frame = new frm09();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public frm09() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(0, 0, 350, 250);
+        setLayout(null);
+        setLocationRelativeTo(null);
+
+        JLabel lblNumero = new JLabel("Número (4 cifras) :");
+        lblNumero.setBounds(50, 50, 120, 30);
+        getContentPane().add(lblNumero);
+
+        JLabel lblSuma = new JLabel("Suma de cifras :");
+        lblSuma.setBounds(50, 100, 120, 30);
+        getContentPane().add(lblSuma);
+
+        txtNumero = new JTextField();
+        txtNumero.setBounds(180, 50, 80, 30);
+        txtNumero.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtNumero);
+
+        txtSuma = new JTextField();
+        txtSuma.setBounds(180, 100, 80, 30);
+        txtSuma.setFocusable(false);
+        txtSuma.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtSuma);
+
+        JButton btnCalcular = new JButton("Calcular");
+        btnCalcular.setBounds(125, 150, 100, 30);
+        getContentPane().add(btnCalcular);
+
+        btnCalcular.addActionListener(e -> btnCalcular_actionPerformed());
+    }
+
+    protected void btnCalcular_actionPerformed() {
+        try {
+            int numero = Integer.parseInt(txtNumero.getText());
+
+            int millares = numero / 1000;
+            int centenas = (numero % 1000) / 100;
+            int decenas = (numero % 100) / 10;
+            int unidades = numero % 10;
+
+            int suma = millares + centenas + decenas + unidades;
+
+            txtSuma.setText(String.valueOf(suma));
+        } catch (NumberFormatException e) {
+            txtSuma.setText("Error");
+        }
+    }
 }
