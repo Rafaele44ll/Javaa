6a06446f3fc8afce6a8596d40563044dfde7445b
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-11-01 16:34:55 -0500

    java

--- src/repetitivos/frm03.java
+++ src/repetitivos/frm03.java
@@ -1,5 +1,69 @@
 package repetitivos;
 
-public class frm03 {
+import java.awt.EventQueue;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
+public class frm03 extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private JTextField txtNumero;
+    private JTextField txtCantidadDivisores;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    frm03 frame = new frm03();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @SuppressWarnings("unused")
+    public frm03() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(0, 0, 300, 200);
+        setLayout(null);
+        setLocationRelativeTo(null);
+
+        JLabel lblNumero = new JLabel("Número:");
+        lblNumero.setBounds(20, 20, 100, 30);
+        add(lblNumero);
+
+        txtNumero = new JTextField();
+        txtNumero.setBounds(120, 20, 150, 30);
+        add(txtNumero);
+
+        JButton btnCalcular = new JButton("Calcular Divisores");
+        btnCalcular.setBounds(20, 70, 250, 30);
+        btnCalcular.addActionListener(e -> calcularDivisores());
+        add(btnCalcular);
+
+        JLabel lblCantidadDivisores = new JLabel("Cantidad de Divisores:");
+        lblCantidadDivisores.setBounds(20, 120, 150, 30);
+        add(lblCantidadDivisores);
+
+        txtCantidadDivisores = new JTextField();
+        txtCantidadDivisores.setBounds(170, 120, 100, 30);
+        txtCantidadDivisores.setEditable(false);
+        add(txtCantidadDivisores);
+    }
+
+    private void calcularDivisores() {
+        int numero = Integer.parseInt(txtNumero.getText());
+        int cantidadDivisores = 0;
+
+        for (int i = 1; i <= numero; i++) {
+            if (numero % i == 0) {
+                cantidadDivisores++;
+            }
+        }
+
+        txtCantidadDivisores.setText(String.valueOf(cantidadDivisores));
+    }
 }
