76858ed2e88cd3decb13c15bc4a85d7a5d3180d2
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-11-01 23:51:18 -0500

    java

--- src/recursivos/frm03.java
+++ src/recursivos/frm03.java
@@ -1,5 +1,63 @@
 package recursivos;
 
-public class frm03 {
+import java.awt.EventQueue;
+import java.awt.Insets;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
+public class frm03 extends JFrame {
+    private static final long serialVersionUID = 1L;
+    JTextField txtEntrada;
+    JTextField txtResultado;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    frm03 frame = new frm03();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @SuppressWarnings("unused")
+    public frm03() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(0, 0, 300, 200);
+        setLayout(null);
+        setLocationRelativeTo(null);
+
+        txtEntrada = new JTextField();
+        txtEntrada.setBounds(20, 20, 240, 30);
+        txtEntrada.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtEntrada);
+
+        JButton btnCalcular = new JButton("Calcular Suma Natural");
+        btnCalcular.setBounds(20, 60, 200, 30);
+        btnCalcular.addActionListener(e -> calcularSumaNatural());
+        getContentPane().add(btnCalcular);
+
+        txtResultado = new JTextField();
+        txtResultado.setBounds(20, 100, 240, 30);
+        txtResultado.setMargin(new Insets(5, 5, 5, 5));
+        txtResultado.setEditable(false);
+        getContentPane().add(txtResultado);
+    }
+
+    protected void calcularSumaNatural() {
+        String entrada = txtEntrada.getText();
+        int numero = Integer.parseInt(entrada);
+        long resultado = sumaNatural(numero);
+        txtResultado.setText("Suma Natural: " + resultado);
+    }
+
+    protected long sumaNatural(int n) {
+        if (n <= 1)
+            return n;
+        return n + sumaNatural(n - 1);
+    }
 }
